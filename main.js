205394d65c9242b482888ed284f079992c2e9c82
jakechaley <jakechaley@gmail.com>
2021-11-25 16:38:53 -0800

    build out balloonPop out using Html, CSS, and JavaScript

---
+++ main.js
@@ -0,0 +1,137 @@
+
+
+// #region GAME LOGIC AND DATA
+
+// DATA
+let clickCount = 0
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxsize = 300
+let highestPopCount = 0
+let currentPopCount = 0
+let gameLength = 5000
+let clockId = 0
+let timeRemaining = 0
+let currentPlayer = {}
+
+function startGame() {
+
+  document.getElementById("game-controls").classList.remove("hidden")
+  document.getElementById("main-controls").classList.add("hidden")
+  startClock()
+  setTimeout(stopGame, gameLength)
+}
+
+function startClock(){
+  timeRemaining = gameLength
+  drawClock()
+  clockId = setInterval(drawClock, 1000)
+}
+function stopClock(){
+  clearInterval(clockId)
+}
+
+function drawClock(){
+  let countdownElem = document.getElementById('countdown')
+  countdownElem.innerText = (timeRemaining / 1000).toString()
+  timeRemaining -= 1000
+}
+
+
+function inflate(){
+  clickCount ++
+  height += inflationRate
+  width += inflationRate
+  
+  if (height >= maxsize){
+    console.log("pop the balloon")
+    currentPopCount++
+    height = 0;
+    width = 0;
+    
+  }
+  draw()
+}
+
+function draw(){
+  let balloonElement = document.getElementById("balloon")
+  let clickCountElem = document.getElementById("click-count")
+  let popCountElem = document.getElementById('pop-count')
+  let highpopCountElem = document.getElementById('high-pop-count')
+  let playerNameElem = document.getElementById('player-name')
+
+
+  balloonElement.style.height = height + "px"
+  balloonElement.style.width = width + "px"
+  
+  clickCountElem.innerText = clickCount.toString()
+  popCountElem.innerText = currentPopCount.toString()
+  highpopCountElem.innerText = currentPlayer.highScore.toString()
+  playerNameElem.innerText = currentPlayer.name
+  
+}
+
+function stopGame() {
+  console.log("the game is over")
+
+  document.getElementById("main-controls").classList.remove("hidden")
+  document.getElementById("game-controls").classList.add("hidden")
+
+  clickCount = 0
+  height = 120
+  width = 100
+
+  if(currentPopCount > currentPlayer.highScore) {
+    currentPlayer.highScore = currentPopCount
+    savePlayers()
+  }
+    
+
+
+  currentPopCount = 0
+
+  stopClock()
+  draw()
+}
+// #endregion 
+
+let players = [] 
+loadPlayers()
+
+function setPlayer(event) { 
+event.preventDefault()
+let form = event.target 
+let playerName = form.playerName.value
+
+currentPlayer = players.find(player => player.name == playerName)
+
+if(!currentPlayer) {
+    currentPlayer = { name: playerName, highScore: 0 }
+    players.push(currentPlayer)
+    savePlayers() 
+}
+
+
+form.reset()
+document.getElementById("game").classList.remove("hidden")
+form.classList.add("hidden")
+draw()
+}
+
+function changePlayer(){
+  document.getElementById("player-form").classList.remove("hidden")
+  document.getElementById("game").classList.add("hidden")
+}
+
+function savePlayers() {
+  window.localStorage.setItem("players", JSON.stringify(players))
+}
+function loadPlayers() {
+  let playersData = JSON.parse(window.localStorage.getItem("player"))
+  if (!playersData){
+    players = []
+  }else{
+    players = playersData
+  }
+} 
